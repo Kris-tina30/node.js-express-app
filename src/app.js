e076ac6147302c9ea79030d5883e92203440c0ea
Kris-tina30 <kristina.pichkar.15@gmail.com>
2025-03-20 10:31:30 +0100

    use ejs templates, make partials,

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  //   res.send('<p>Home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
-});
-app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
-});
-app.get('/about-us', (req, res) => {
-  res.redirect('./about');
-});
-app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+  });
+});
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {
+    title: 'Create a new blog',
+  });
+});
+app.use((req, res) => {
+  res.status(404).render('404', {
+    title: '404',
+  });
 });
